baa7b0d70c9d13638d7574c3e0b8ed9c7974cd11
notunrandom <notunrandom@proton.me>
2023-10-15 13:50:41 +0200

    Introduce systems of several functions.

---
+++ eigen/system.py
@@ -0,0 +1,14 @@
+import eigen.values
+
+
+def inconsistencies(functions, system):
+    _, tables = system
+    for name, table in tables:
+        if name not in functions.keys():
+            for values in table:
+                *args, result = values
+                yield (name, args + [(result, None)])
+        else:
+            function = functions[name]
+            for inc in eigen.values.inconsistencies(function, table):
+                yield (name, inc)
---
+++ tests/test_system.py
@@ -0,0 +1,44 @@
+from eigen.system import inconsistencies
+
+
+def test_inconsistencies_empty_system():
+
+    system = ('empty', [])
+    assert list(inconsistencies({}, system)) == []
+
+
+def test_inconsistencies_none():
+    addition = ('add', [[13, 17, 30]])
+    system = ('arithmetic', [addition])
+
+    def myadd(x, y):
+        return x + y
+
+    solution = {'add': myadd}
+    assert list(inconsistencies(solution, system)) == []
+
+
+def test_inconsistencies():
+    addition = ('add', [[5, 1, 6], [13, 17, 30]])
+    subtraction = ('subtract', [[5, 1, 4], [13, 17, -4]])
+    multiplication = ('multiply', [[5, 1, 5], [13, 17, 221]])
+    other = ('other', [[1, 6, 99], [12, 12, 12]])
+    system = ('arithmetic', [addition, subtraction, multiplication, other])
+
+    def badadd(x, _):
+        return x + 1
+
+    def badsub(x, _):
+        return x - 1
+
+    def mymult(x, y):
+        return x * y
+
+    solution = {'add': badadd, 'subtract': badsub, 'multiply': mymult}
+
+    result = list(inconsistencies(solution, system))
+    assert len(result) == 4
+    assert ('add', [13, 17, (30, 14)]) in result
+    assert ('subtract', [13, 17, (-4, 12)]) in result
+    assert ('other', [1, 6, (99, None)]) in result
+    assert ('other', [12, 12, (12, None)]) in result
